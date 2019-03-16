b1f9c21fdc752d04f142669e6bc53087497b8027
Jesus Conde <educaleft@gmail.com>
2019-03-16 12:36:05 +0000

    Añadidos enlaces a nuevos SW en index.html

--- sw_offlinepage.js
+++ sw_offlinepage.js
@@ -1,34 +1,26 @@
-var offlinePageCache = 'customOfflinePage';
+var offlinePageCache = "customOfflinePage";
 
 var offlineURL = "offline.html";
 
-this.addEventListener('install', function(event){
+this.addEventListener("install", function(event) {
+  console.log("Instalando Service Worker");
 
-	console.log('Instalando Service Worker');
-
-	event.waitUntil(
-
-		caches.open(offlinePageCache)
-		.then(function(cache){
-			return cache.add(offlineURL);
-		})
-		.then(function(){
-			return self.skipWaiting();
-		})
-
-	);
-
+  event.waitUntil(
+    caches
+      .open(offlinePageCache)
+      .then(function(cache) {
+        return cache.add(offlineURL);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
 });
 
-this.addEventListener('fetch', function(event){
-
-	event.respondWith(
-
-		fetch(event.request)
-		.catch(function(){
-			return caches.match(offlineURL);
-		})
-
-	);
-
+this.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(offlineURL);
+    })
+  );
 });
--- sw_offlinesite.js
+++ sw_offlinesite.js
@@ -1,41 +1,32 @@
-var offlineSiteCache = 'offlineSite';
+var offlineSiteCache = "offlineSite";
 
 var resourcesToCache = [
-	'index.html',
-	'about.html',
-	'stylesheets/main.css',
-	'stylesheets/parallax.css',
-	'stylesheets/main-responsive.css'
-	
+  "index.html",
+  "offline.html",
+  "stylesheets/main.css",
+  "stylesheets/parallax.css",
+  "stylesheets/main-responsive.css"
 ];
 
-this.addEventListener('install', function(event){
+this.addEventListener("install", function(event) {
+  console.log("Instalando Service Worker");
 
-	console.log('Instalando Service Worker');
-
-	event.waitUntil(
-
-		caches.open(offlineSiteCache)
-		.then(function(cache){
-			return cache.addAll(resourcesToCache);
-		})
-		.then(function(){
-			return self.skipWaiting();
-		})
-
-	);
-
+  event.waitUntil(
+    caches
+      .open(offlineSiteCache)
+      .then(function(cache) {
+        return cache.addAll(resourcesToCache);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
 });
 
-this.addEventListener('fetch', function(event){
-
-	event.respondWith(
-
-		fetch(event.request)
-		.catch(function(){
-			return caches.match(event.request);
-		})
-
-	);
-
+this.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
